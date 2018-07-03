aa8e0a607ca4bdeb7548e1618e42cf128c0bb8d6
Vladimir Saakyan <polossless@gmail.com>
2018-07-03 20:00:06 +0300

    Delete analitic

--- src/components/Html.js
+++ src/components/Html.js
@@ -10,7 +10,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import serialize from 'serialize-javascript';
-import config from '../config';
 
 /* eslint-disable react/no-danger */
 
@@ -63,24 +62,6 @@ class Html extends React.Component {
             dangerouslySetInnerHTML={{ __html: `window.App=${serialize(app)}` }}
           />
           {scripts.map(script => <script key={script} src={script} />)}
-          {config.analytics.googleTrackingId && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html:
-                  'window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;' +
-                  `ga('create','${
-                    config.analytics.googleTrackingId
-                  }','auto');ga('send','pageview')`,
-              }}
-            />
-          )}
-          {config.analytics.googleTrackingId && (
-            <script
-              src="https://www.google-analytics.com/analytics.js"
-              async
-              defer
-            />
-          )}
         </body>
       </html>
     );
