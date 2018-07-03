55b7e37a869fd8971240ec2ad9dc8e49b38343d9
Max Osadchyi <osadchyimax17@gmail.com>
2018-07-03 20:11:10 +0300

    Delete build

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -25,7 +25,7 @@ class Layout extends React.Component {
     return (
       <div>
         <Header />
-        {/* <Sidebar /> */}
+        <Sidebar />
         <Main />
         <Footer />
       </div>
