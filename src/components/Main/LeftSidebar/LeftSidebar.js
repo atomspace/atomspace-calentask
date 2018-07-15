68030f4a0781fa4d910fc7e2f43053ad6d7f1530
Max Osadchyi <osadchyimax17@gmail.com>
2018-07-15 17:00:26 +0300

    update calendar

--- src/components/Main/LeftSidebar/LeftSidebar.js
+++ src/components/Main/LeftSidebar/LeftSidebar.js
@@ -2,13 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
-import IconButton from '@material-ui/core/IconButton';
-import Hidden from '@material-ui/core/Hidden';
 import Divider from '@material-ui/core/Divider';
-import MenuIcon from '@material-ui/icons/Menu';
 import { mailFolderListItems, otherMailFolderListItems } from './tileData/tileData';
 
 const drawerWidth = 240;
@@ -50,26 +45,12 @@ const styles = theme => ({
 });
 
 class LeftSidebar extends React.Component {
-  state = {
-    mobileOpen: false,
-  };
-
   handleDrawerToggle = () => {
     this.setState(state => ({ mobileOpen: !state.mobileOpen }));
   };
 
   render() {
-    const { classes, theme } = this.props;
-
-    const drawer = (
-      <div>
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>{mailFolderListItems}</List>
-        <Divider />
-        <List>{otherMailFolderListItems}</List>
-      </div>
-    );
+    const { classes } = this.props;
 
     return (
       <div className={classes.root}>
@@ -91,7 +72,6 @@ class LeftSidebar extends React.Component {
 
 LeftSidebar.propTypes = {
   classes: PropTypes.shape({}).isRequired,
-  theme: PropTypes.shape({}).isRequired,
 };
 
 export default withStyles(styles, { withTheme: true })(LeftSidebar);
