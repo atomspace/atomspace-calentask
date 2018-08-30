27c42f728571898858c3c2d43ad1627a86088748
Mishawaka <budishmishka@gmail.com>
2018-08-30 19:43:07 +0300

    Sending data from server to page and added moment.js

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -7,24 +7,26 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 import Main from '../../components/Main/Main';
 import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
+import moment from 'moment';
 
 const MessageQuery = gql`
   {
     messages(room: "Cherry") {
       text
+      time
       id
     }
   }
 `;
 
-class Home extends React.Component {
+class Home extends Component {
   static propTypes = {
     news: PropTypes.arrayOf(
       PropTypes.shape({
@@ -41,15 +43,22 @@ class Home extends React.Component {
       return <div>Loading...</div>;
     }
     return data.messages.map(message => (
-      <li key={message.id}>{message.text}</li>
+      <li key={message.id}>{moment(message.time).format('HH:mm:ss')}</li>
     ));
   }
 
   render() {
+    const { data } = this.props;
+    const messages = data.messages || [];
+    const msgTexts = messages.map(message => message.text);
+    console.log(msgTexts);
+
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <Main />
+          <div>
+            <Main />
+          </div>
         </div>
       </div>
     );
