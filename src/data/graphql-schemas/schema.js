44ada8edfbe77beae29ff9219af548492ab37307
Mishawaka <budishmishka@gmail.com>
2018-09-09 20:47:36 +0300

    Changed query

--- src/data/graphql-schemas/schema.js
+++ src/data/graphql-schemas/schema.js
@@ -25,40 +25,41 @@ const MessageType = new GraphQLObjectType({
   }),
 });
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      message: {
-        type: MessageType,
-        args: {
-          room: {
-            type: GraphQLString,
-          },
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    message: {
+      type: MessageType,
+      args: {
+        room: {
+          type: GraphQLString,
         },
-        resolve: (root, { room }) =>
-          new Promise((resolve, reject) => {
-            Message.findOne({ from: room }, (err, message) => {
-              err ? reject(err) : resolve(message);
-            });
-          }),
       },
-      messages: {
-        type: new GraphQLList(MessageType),
-        args: {
-          room: {
-            type: GraphQLString,
-          },
-        },
-        resolve: (root, { room }) =>
-          new Promise((resolve, reject) => {
-            Message.find({ from: room }, (err, messages) => {
-              err ? reject(err) : resolve(messages);
-            });
-          }),
-      },
+      resolve: (root, { room }) =>
+        new Promise((resolve, reject) => {
+          Message.findOne({ from: room }, (err, message) => {
+            err ? reject(err) : resolve(message);
+          });
+        }),
     },
-  }),
+    messages: {
+      type: new GraphQLList(MessageType),
+      args: {
+        room: {
+          type: GraphQLString,
+        },
+      },
+      resolve: (root, { room }) => new Promise((resolve, reject) => {
+        Message.find({ from: room }, (err, messages) => {
+          err ? reject(err) : resolve(messages);
+        });
+      })
+    }
+  }
+});
+
+const schema = new GraphQLSchema({
+  query: RootQuery
 });
 
 export default schema;
