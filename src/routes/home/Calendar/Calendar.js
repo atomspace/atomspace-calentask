31186e875faf6870083a6f6a39ea4ddd243539c1
Vladimir Saakyan <polossless@gmail.com>
2018-10-09 19:52:48 +0300

    Fix css

--- src/routes/home/Calendar/Calendar.js
+++ src/routes/home/Calendar/Calendar.js
@@ -36,72 +36,54 @@ const day = [
 
 class Calendar extends Component {
   displayTime() {
-    var arr = [];
+    const arr = [];
 
-    for(var i = 0; i < 34; i++) {
+    for (let i = 0; i < 34; i++) {
       arr.push(
         <div key={i} className={s.flex}>
           <div className={s.boxtime}>00:00</div>
-          <div className={`${s.box} ${s.wide}`}>
-
-          </div>
-        </div>
+          <div className={`${s.box} ${s.wide}`} />
+        </div>,
       );
     }
     return arr;
   }
 
   render() {
-    return(
+    return (
       <div className={s.Calendar}>
         <div className={`${s.flex} ${s.con_box__hor} ${s.abs}`}>
+          <div className={`${s.marginLeft}`}>
+            <div className={s.box} style={{ border: 0, marginLeft: '13%' }}>
+              333
+            </div>
+            <div />
+          </div>
+          <div className={s.flex_col}>
+            <div className={s.box}>Понедельник</div>
+            <div className={s.box}>Вторник</div>
+            <div className={s.box}>Среда</div>
+            <div className={s.box}>Четверг</div>
+            <div className={s.box}>Пятница</div>
+            <div className={s.box}>Суббота</div>
+            <div className={s.box}>Воскресенье</div>
+          </div>
+        </div>
+
         <div>
-          <div className={s.box} style={{border: 0, marginLeft: '13%'}}>333</div>
-          <div>
-
-          </div>
+          <div className={`${s.abs_div} ${s.colMonday}`} />
+          <div className={`${s.abs_div} ${s.colTuesday}`} />
+          <div className={`${s.abs_div} ${s.colWednesday}`} />
+          <div className={`${s.abs_div} ${s.colThursday}`} />
+          <div className={`${s.abs_div} ${s.colFriday}`} />
+          <div className={`${s.abs_div} ${s.colSaturday}`} />
+          <div className={`${s.abs_div} ${s.colSunday}`} />
         </div>
-        <div className={s.flex_col}>
-          <div className={s.box}>
-            Понедельник
-          </div>
-          <div className={s.box}>
-            Вторник
-          </div>
-          <div className={s.box}>
-            Среда
-          </div>
-          <div className={s.box}>
-            Четверг
-          </div>
-          <div className={s.box}>
-            Пятница
-          </div>
-          <div className={s.box}>
-            Суббота
-          </div>
-          <div className={s.box}>
-            Воскресенье
-          </div>
+
+        <div className={`${s.con_box__vert} ${s.padd}`}>
+          {this.displayTime()}
         </div>
       </div>
-
-      <div>
-        <div className={`${s.abs_div} ${s.colMonday}`}>
-        <div className={`${s.abs_div} ${s.colTuesday}`}></div>
-        <div className={`${s.abs_div} ${s.colWednesday}`}></div>
-        <div className={`${s.abs_div} ${s.colThursday}`}></div>
-        <div className={`${s.abs_div} ${s.colFriday}`}></div>
-        <div className={`${s.abs_div} ${s.colSaturday}`}></div>
-        <div className={`${s.abs_div} ${s.colSunday}`}></div>
-
-        </div>
-      </div>
-
-      <div className={`${s.con_box__vert} ${s.padd}`}>
-        {this.displayTime()}
-      </div>
-    </div>
     );
   }
 }
